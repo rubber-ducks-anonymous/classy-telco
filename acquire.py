214329a2718ac545f046c962e08eddfa9cffa8af
RaulCPena <raul.c.pena@gmail.com>
2020-04-23 10:58:16 -0500

    added payment_type to sql query

--- acquire.py
+++ acquire.py
@@ -10,11 +10,10 @@ def get_db_url(dbname) -> str:
 
 def get_telco_data():
     sql_query = """
-    SELECT c.*, ct.contract_type, ist.internet_service_type 
+    SELECT c.*, ct.contract_type, ist.internet_service_type, pt.payment_type
     FROM customers c
     JOIN contract_types ct ON ct.contract_type_id = c.contract_type_id
     JOIN internet_service_types ist ON ist.internet_service_type_id = c.internet_service_type_id
     JOIN payment_types pt ON pt.payment_type_id = c.payment_type_id
-
     """
     return pd.read_sql(sql_query, get_db_url('telco_churn'))
